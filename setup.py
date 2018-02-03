62b006a656c69c607e0ac0908e4d0dfa7ba39a99
Jose Sebastián Canós <ep3p@users.noreply.github.com>
2018-02-03 20:46:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Jose Sebastián Canós',
     author_email='elpoliticamentecorrecto@gmail.com',
     packages=['instagram_monitor'],
-    install_requires=['networkx', 'pymongo', 'requests'],
+    install_requires=['networkx', 'pymongo', 'requests', 'matplotlib'],
     dependency_links=['https://codeload.github.com/ping/instagram_private_api/tar.gz/1.3.3'],
     entry_points={'console_scripts': 
         ['instagram_monitor = instagram_monitor.__main__:main']})
